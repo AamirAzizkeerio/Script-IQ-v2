ee6e0cef1285dcf950029c2bc5f2635fefcbb7ae
AamirAzizkeerio <munibaziz6@gmail.com>
2026-07-06 19:05:24 +0500

    Import and initialize Paddle client in main.tsx
    Added initialization of Paddle client.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './components/AuthContext';
+import { initializePaddleClient } from './paddle';
 import './index.css';
+
+initializePaddleClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
